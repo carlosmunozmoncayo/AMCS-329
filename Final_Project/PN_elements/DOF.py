6d8db7795bf0bf35f7c889d9a707a4eb4a1e42f7
carlosmunozmoncayo <carlos.munozmoncayo@kaust.edu.sa>
2022-11-21 00:44:39 +0300

    Add and update all functions for finding DOF
    Still need to couple everything together. Pull back-push forward
    function also updated.

--- Final_Project/PN_elements/DOF.py
+++ Final_Project/PN_elements/DOF.py
@@ -1,9 +1,12 @@
 #Defining degrees of freedom that are not at the vertices
+import numpy as np
+from itertools import combinations
+from pull_back import pull_back_forward
 
-import itertools
 
 def get_edges_DOF(triangulation, points, k):
-
+    #Get degrees of freedom over all the domain. This function returns the DOF
+    #that are not vertices or inner points for each triangle.
     #############
     #k is the number of interior poins at each edge, which is
     #1 less than the degree of the polynomials from our basis
@@ -21,7 +24,7 @@ def get_edges_DOF(triangulation, points, k):
     coeffs = np.linspace(0., 1., k+2)[1:-1] 
 
     for triangle in triangulation.simplices:
-        sets_of_pairs = [set(i) for i in itertools.combinations(triangle,2)] #Combinations of 2 points (list of sets of 2 indices)
+        sets_of_pairs = [set(i) for i in combinations(triangle,2)] #Combinations of 2 points (list of sets of 2 indices)
         for pair in sets_of_pairs:
             if pair not in indexes_connected_points:
                 indexes_connected_points.append(pair)
@@ -30,3 +33,36 @@ def get_edges_DOF(triangulation, points, k):
                 for lamb in coeffs:
                     DOF_on_edges.append(lamb*x+(1-lamb)*y)
     return DOF_on_edges
+
+def get_inner_DOF(triangulation, points, DOF_ref_inner):
+    DOF_inner = []
+    for triangle in triangulation.simplices:
+        A,b,_,_,_ = pull_back_forward(triangle,points)
+        for x in DOF_ref_inner:
+            DOF_inner.append(A@x+b)
+    return np.array(DOF_inner)
+
+def get_all_DOF_reference(poly_degree):
+    #Get all the degrees of freedom in the reference triangle
+    #Also returns just the inner points for practical purposes
+    ##########################################################
+
+    #Adding horizontal and vertical points
+    DOF_reference = []
+    DOF_reference_inner = []
+    delta = 1./poly_degree
+    x,y = -delta, -delta
+    for iy in range(poly_degree+1):
+        y += delta
+        for jx in range(poly_degree+1-iy):
+            x += delta
+            DOF_reference.append((x,y))
+            if x>0. and y >0. and y+x<.9999 :
+                DOF_reference_inner.append((x,y))
+        x = -delta
+    return np.array(DOF_reference), np.array(DOF_reference_inner)
+
+        
+
+    
+
--- Final_Project/PN_elements/pull_back.py
+++ Final_Project/PN_elements/pull_back.py
@@ -12,18 +12,18 @@ import numpy as np
 #Assuming an affine push forward function, i.e.,
 #x = Ft(xr) = A xr+b
 
-def p_back_forward(triangle,points):
+def pull_back_forward(triangle,points):
     #Returns important quantities given a triangle and list of points
     #output: Jac(F_T), |det(Jac(F_T)|, Jac^{-1}(F_T), |det(Jac^{-1}(F_T)|
 
     #Extremely inefficient btw, it would be better to receive the three points
     #of the triangle directly isntead of passing the list of points each time.
     #It suffices for our simple applications anyway.
-    [x1,x2,x3] = [points[triangle[i]] for i in range(3)]
-    b = x2
-    A = np.dstack((x2-x1,x3-x1))
-    DFT_inv = np.linalg.inv(A)
-    return A, np.abs(np.linalg.det(A)), DFT_inv, np.abs(np.linalg.det(DFT_inv))
+    [x1,x2,x3] = points[triangle]
+    b = x1
+    A = np.dstack((x2-x1,x3-x1)).reshape(-1,2)
+    A_inv = np.linalg.inv(A)
+    return A, b, A_inv, np.abs(np.linalg.det(A)), np.abs(np.linalg.det(A_inv))
 
 
 
