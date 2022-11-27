b2e6d944677250b389524f90c94802dec90e7667
carlosmunozmoncayo <carlos.munozmoncayo@kaust.edu.sa>
2022-11-27 23:17:48 +0300

    Add integration of shape functions (and grads)

--- Final_Project/PN_elements/quadrature.py
+++ Final_Project/PN_elements/quadrature.py
@@ -11,13 +11,25 @@ def integrate_over_reference(f,deg):
     gauss_leg = 0.
     for i in range(len(xi)):
         for j in range(len(eta)):
-            gauss_leg += wxi[i]*weta[j]*f(x[i],y[i][j])*(1-xi[i])/8.
+            gauss_leg += wxi[i]*weta[j]*f(np.array(x[i],y[i][j]))*(1-xi[i])/8.
     return gauss_leg
 
 #def integrate_contribution_any_triangle(triangle,DOF, f,i):
     #Integrate f(x)*phi_i(x) over a triangle
     #i denotes the DOF in triangle corresponding to 
     
+def integrate_f_phi(shape_function,affine_mapping,f,degree):
+    def compose_f_phi_affine(x_ref):
+            x = affine_mapping.A@x_ref + affine_mapping.b
+        return f(x)*shape.phi(x)
+    return affine_mapping.detA*integrate_over_reference(compose_f_phi_affine,degree)
+
+def integrate_gradphiI_dot_gradphiJ(shapef_i,shapef_j,affine_mapping,degree):
+    def compose_grad_phi_affine(x_ref):
+        x = affine_mapping.A@x_ref + affine_mapping.b
+        return np.dot(shapef_i.grad_phi(x),shapef_j.grad_phi(x))
+    return affine_mapping.detA*integrate_over_reference(compose_grad_phi_affine,degree)
+
 
     
 
